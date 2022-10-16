602bbfaadb446409c3b0d7ecad6a8cc29f7e4be0
Nadezhda <90304907+Hopechka@users.noreply.github.com>
2022-10-16 14:04:38 +0600

    init: nevatrip-test-task

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export interface ITours {
+  id?: number
+  title: string
+  price: number,
+  description: string[]
+  duration: string,
+  schedule: string[]
+  category: string
+  image: string
+  details: string
+}
